5f41b1e357f04d9b8d395dccd64e067b20a4463a
sarath.krishnan@slalom.com-at-617780835636 <sarath.krishnan@S01423-MBPR.local>
2020-04-16 19:37:26 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const core = require('@actions/core');
+const axios = require('axios');
+
+try{
+    const workSpaceName = core.getInput('workSpaceName');
+    const organizationName = core.getInput('organizationName');
+    const token = core.getInput('terraformToken');
+    let request = { data : { attributes: { name : workSpaceName, type: "workspaces"}}};
+    console.log("request:" + JSON.stringify(request));
+
+    const hashiCorpEndpoint = "https://app.terraform.io/api/v2/organizations/"+organizationName+"/workspaces";
+    const options = {
+        headers: {'Content-Type': 'application/vnd.api+json',
+                  'Authorization': 'Basic '+token
+                }       
+    };
+
+    axios.post(hashiCorpEndpoint, request, options)
+      .then((response) => {
+        console.log("success:"+JSON.stringify(response));
+      }, (error) => {
+        console.error("error:"+JSON.stringify(error));
+        output = error;
+      });
+
+} catch(error){
+    core.setFailed(error.message);
+}
